8b7381863705a4ff0bc84dc127b9d3cd676513a5
khaled196 <khalled.jooma@yahoo.com>
2023-04-13 15:29:07 +0000

    Mingkai update

--- beacon-2-import.py
+++ beacon-2-import.py
@@ -21,7 +21,7 @@ from argparse import Namespace
 #
 # TODO can we install it like this
 import asyncpg
-from beacon_api.utils.db_load import BeaconDB
+from db_load import BeaconDB
 from requests import Response
 from bioblend.galaxy import GalaxyInstance
 
@@ -32,70 +32,102 @@ from pymongo import MongoClient
 import conf
 import json
 
-class BeaconExtendedDB(BeaconDB):
-    """
-    This class is used to hijack beacons internal database class from the "beacon_api.utils" package
-    """
 
-    async def get_variant_indices(self, start: int, ref: str, alt: str) -> List[int]:
-        """
-        Returns database indices of all occurrences of the given variant
 
-            Parameters:
-                start (int): start position of the variant
-                ref (str): sequence in the reference
-                alt (str): sequence of the variant
+class BeaconExtendedDB:
+    def __int__(self):
+        self.client=MongoClient()
+    async def connection(self):
+        self.client = MongoClient(
 
-            Returns:
-                list of matching indices (possibly empty)
-        """
-
-        self._conn: asyncpg.Connection
-        rows = await self._conn.fetch(
-            f"SELECT index FROM beacon_data_table where start={start} AND reference='{ref}' and alternate='{alt}'")
-        return [row["index"] for row in rows]
+            # connect to MongoDB database
+            "mongodb://{}:{}@{}:{}/{}?authSource={}".format(
+                conf.database_user,
+                conf.database_password,
+                conf.database_host,
+                conf.database_port,
+                conf.database_name,
+                conf.database_auth_source,
+            )
+        )
+    async def createCollection(self):
+        self.client.beacon.createCollection("analyses")
+        self.client.beacon.createCollection("biosamples")
+        self.client.beacon.createCollection("cohorts")
+        self.client.beacon.createCollection("datasets")
+        self.client.beacon.createCollection("genomicVariations")
+        self.client.beacon.createCollection("individuals")
+        self.client.beacon.createCollection("runs")
 
     async def clear_database(self):
-        """
-        Removes all data from beacons internal database
-
-        The database schema will remain unchanged
-        """
-        await self._conn.execute("DELETE FROM beacon_data_table")
-        await self._conn.execute("DELETE FROM beacon_dataset_table")
-        await self._conn.execute("DELETE FROM beacon_dataset_counts_table")
-
+        pass
 
     async def update_dataset_counts(self):
-        """
-        Calculates and sets actual counts for the accumulated datasets
-
-            Parameters:
-                None
-            
-            Returns:
-                Nothing
-        """
-        records: List[asyncpg.Record] = await self._conn.fetch(
-            "SELECT datasetid, COUNT(*) as count, SUM(callcount) as callcount FROM beacon_data_table GROUP BY datasetid")
-
-        # clear beacon_dataset_counts table
-        # beacon_init function will add multiple lines for the same dataset, one for each file that is imported
-        await self._conn.execute("DELETE FROM beacon_dataset_counts_table")
-
-        # persist the actual count values
-        for dataset in records:
-            await self._conn.execute(
-                f"INSERT INTO beacon_dataset_counts_table(datasetid, callcount, variantcount) " +
-                f"VALUES('{dataset['datasetid']}', {dataset['count']}, {dataset['callcount']})")
-
-        # hide the sample count
-        await self._conn.execute("UPDATE beacon_dataset_table SET samplecount = NULL")
-
+        pass
+# class BeaconExtendedDB(BeaconDB):
+#     """
+#     This class is used to hijack beacons internal database class from the "beacon_api.utils" package
+#     """
+#
+#     async def get_variant_indices(self, start: int, ref: str, alt: str) -> List[int]:
+#         """
+#         Returns database indices of all occurrences of the given variant
+#
+#             Parameters:
+#                 start (int): start position of the variant
+#                 ref (str): sequence in the reference
+#                 alt (str): sequence of the variant
+#
+#             Returns:
+#                 list of matching indices (possibly empty)
+#         """
+#
+#         self._conn: asyncpg.Connection
+#         rows = await self._conn.fetch(
+#             f"SELECT index FROM beacon_data_table where start={start} AND reference='{ref}' and alternate='{alt}'")
+#         return [row["index"] for row in rows]
+#
+#     async def clear_database(self):
+#         """
+#         Removes all data from beacons internal database
+#
+#         The database schema will remain unchanged
+#         """
+#         await self._conn.execute("DELETE FROM beacon_data_table")
+#         await self._conn.execute("DELETE FROM beacon_dataset_table")
+#         await self._conn.execute("DELETE FROM beacon_dataset_counts_table")
+#
+#     async def update_dataset_counts(self):
+#         """
+#         Calculates and sets actual counts for the accumulated datasets
+#
+#             Parameters:
+#                 None
+#
+#             Returns:
+#                 Nothing
+#         """
+#         records: List[asyncpg.Record] = await self._conn.fetch(
+#             "SELECT datasetid, COUNT(*) as count, SUM(callcount) as callcount FROM beacon_data_table GROUP BY datasetid")
+#
+#         # clear beacon_dataset_counts table
+#         # beacon_init function will add multiple lines for the same dataset, one for each file that is imported
+#         await self._conn.execute("DELETE FROM beacon_dataset_counts_table")
+#
+#         # persist the actual count values
+#         for dataset in records:
+#             await self._conn.execute(
+#                 f"INSERT INTO beacon_dataset_counts_table(datasetid, callcount, variantcount) " +
+#                 f"VALUES('{dataset['datasetid']}', {dataset['count']}, {dataset['callcount']})")
+#
+#         # hide the sample count
+#         await self._conn.execute("UPDATE beacon_dataset_table SET samplecount = NULL")
 
 
 # global variable for beacon connection
 db: BeaconExtendedDB = BeaconExtendedDB()
+loop = asyncio.get_event_loop()
+
 
 def parse_arguments() -> Namespace:
     """
@@ -111,7 +143,7 @@ def parse_arguments() -> Namespace:
     subparsers = parser.add_subparsers(dest='command')
     subparsers.required = True
 
-    # arguments controlling output 
+    # arguments controlling output
     parser.add_argument("-v", "--verbosity", action="count", default=0,
                         help="log verbosity, can be repeated up to three times")
 
@@ -129,9 +161,10 @@ def parse_arguments() -> Namespace:
     parser_rebuild.add_argument("-o", "--origins-file", type=str, metavar="", default="/tmp/variant-origins.txt",
                                 dest="origins_file",
                                 help="full file path of where variant origins should be stored (if enabled)")
-    
+
     # database connection
-    parser_rebuild.add_argument("-A", "--conf-auth-source", type=str, metavar="", default="admin", dest="database_auth_source",
+    parser_rebuild.add_argument("-A", "--conf-auth-source", type=str, metavar="", default="admin",
+                                dest="database_auth_source",
                                 help="auth source for the beacon database")
     parser_rebuild.add_argument("-H", "--conf-host", type=str, metavar="", default="127.0.0.1", dest="database_host",
                                 help="hostname/IP of the beacon database")
@@ -139,7 +172,8 @@ def parse_arguments() -> Namespace:
                                 help="port of the beacon database")
     parser_rebuild.add_argument("-U", "--conf-user", type=str, metavar="", default="root", dest="database_user",
                                 help="login user for the beacon database")
-    parser_rebuild.add_argument("-W", "--conf-password", type=str, metavar="", default="example", dest="database_password",
+    parser_rebuild.add_argument("-W", "--conf-password", type=str, metavar="", default="example",
+                                dest="database_password",
                                 help="login password for the beacon database")
     parser_rebuild.add_argument("-N", "--conf-name", type=str, metavar="", default="beacon", dest="database_name",
                                 help="name of the beacon database")
@@ -187,6 +221,7 @@ def string_as_bool(string: Any) -> bool:
     else:
         return False
 
+
 def set_up_galaxy_instance(galaxy_url: str, galaxy_key: str) -> GalaxyInstance:
     """
     Returns a galaxy instance with the given URL and api key.
@@ -206,7 +241,7 @@ def set_up_galaxy_instance(galaxy_url: str, galaxy_key: str) -> GalaxyInstance:
     try:
         gi = GalaxyInstance(galaxy_url, key=galaxy_key)
     except Exception as e:
-        # if galaxy_url does not follow the scheme <protocol>://<host>:<port>, GalaxyInstance attempts guessing the URL 
+        # if galaxy_url does not follow the scheme <protocol>://<host>:<port>, GalaxyInstance attempts guessing the URL
         # this exception is thrown when neither "http://<galaxy_url>:80" nor "https://<galaxy_url>:443" are accessible
         logging.critical(f"failed to guess URL from \"{galaxy_url}\" - {e}")
         exit(2)
@@ -252,7 +287,8 @@ def get_beacon_histories(gi: GalaxyInstance) -> List[str]:
 
     # get histories from galaxy api
     # URL is used because the name filter is not supported by bioblend as of now
-    response: Response = gi.make_get_request(f"{gi.base_url}/api/histories?q=name&qv=Beacon%20Export%20%F0%9F%93%A1&all=true&deleted=false")
+    response: Response = gi.make_get_request(
+        f"{gi.base_url}/api/histories?q=name&qv=Beacon%20Export%20%F0%9F%93%A1&all=true&deleted=false")
 
     # check if the reuest was successful
     if response.status_code != 200:
@@ -270,7 +306,8 @@ def get_beacon_histories(gi: GalaxyInstance) -> List[str]:
 
         # skip adding the history if beacon_enabled is not set for the owner account
         history_user_preferences: Dict[str, str] = user_details["preferences"]
-        if "beacon_enabled" not in history_user_preferences or not string_as_bool(history_user_preferences["beacon_enabled"]):
+        if "beacon_enabled" not in history_user_preferences or not string_as_bool(
+                history_user_preferences["beacon_enabled"]):
             continue
 
         history_ids.append(history["id"])
@@ -456,7 +493,7 @@ def prepare_metadata_file(dataset: GalaxyDataset, output_path: str) -> None:
         update_date_time=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         call_count=0,
         version="v0.1",
-        variant_count=0 # the variant count will be updated after all datasets have been processed 
+        variant_count=0  # the variant count will be updated after all datasets have been processed
     )
 
     with open(output_path, 'w', encoding='utf-8') as f:
@@ -483,8 +520,7 @@ def download_dataset(gi: GalaxyInstance, dataset: GalaxyDataset, filename: str) 
         logging.critical(f"something went wrong while downloading file - {e}")
 
 
-
-def import_to_mongodb(BFF_files):
+def import_to_mongodb(BFF_files: list):
     """
     Import a dataset to beacon
 
@@ -507,30 +543,27 @@ def import_to_mongodb(BFF_files):
 
     """
 
-
-
-    Entities = ['analyses_file', 'biosamples_file', 'cohorts_file', 'datasets_file', 'genomicVariations_file', 'individuals_file', 'runs_file']
+    Entities = ['analyses_file', 'biosamples_file', 'cohorts_file', 'datasets_file', 'genomicVariations_file',
+                'individuals_file', 'runs_file']
+    global db
     # Import  BFF files
     for i in range(len(BFF_files)):
         with open(BFF_files[i]) as f:
             data = json.load(f)
             collection_name = Entities[i].split("_")[0]
-            client.beacon[collection_name].insert_many(data)
-            
-            
-            
+            db.client.beacon[collection_name].insert_many(data)
+
         # Create indexes
-    client.beacon.analyses.create_index([("$**", "text")])
-    client.beacon.biosamples.create_index([("$**", "text")])
-    client.beacon.cohorts.create_index([("$**", "text")])
-    client.beacon.datasets.create_index([("$**", "text")])
-    client.beacon.genomicVariations.create_index([("$**", "text")])
-    client.beacon.individuals.create_index([("$**", "text")])
-    client.beacon.runs.create_index([("$**", "text")])
+    db.client.beacon.analyses.create_index([("$**", "text")])
+    db.client.beacon.biosamples.create_index([("$**", "text")])
+    db.client.beacon.cohorts.create_index([("$**", "text")])
+    db.client.beacon.datasets.create_index([("$**", "text")])
+    db.client.beacon.genomicVariations.create_index([("$**", "text")])
+    db.client.beacon.individuals.create_index([("$**", "text")])
+    db.client.beacon.runs.create_index([("$**", "text")])
 
 
-
-async def persist_variant_origins(dataset_id: str, dataset: VCF, record):
+async def persist_variant_origins(dataset_id: str, dataset: str, record):
     """
     Maps dataset_id to variant index in a separate file (which is hard-coded)
 
@@ -549,12 +582,22 @@ async def persist_variant_origins(dataset_id: str, dataset: VCF, record):
         Returns:
             Nothing.
     """
+    global db
+    with open('./genomicVariations.json') as j_f:
+        data = json.load(j_f)
+        for variant in data:
+            ALT = variant['alternateBases']
+            start = variant['position']['start'][0]
+            REF = variant['referenceBases']
+            res=db.client.genomicVariations.find({"alternateBases":ALT},{"start":start},{"referenceBases":REF})
+            record.write(f'{res["_id"]}{dataset_id}\n')
 
-    variant: Variant
-    for variant in dataset:
-        for alt in variant.ALT:
-            for index in await db.get_variant_indices(variant.start, variant.REF, alt):
-                record.write(f"{index} {dataset_id}\n")
+
+    # variant: Variant
+    # for variant in dataset:
+    #     for alt in variant.ALT:
+    #         for index in await db.get_variant_indices(variant.start, variant.REF, alt):
+    #             record.write(f"{index} {dataset_id}\n")
 
 
 async def update_variant_counts():
@@ -563,7 +606,6 @@ async def update_variant_counts():
     """
 
     await db.update_dataset_counts()
-
 
 
 def cleanup(dataset_file: str, metadata_file: str):
@@ -590,20 +632,7 @@ def command_rebuild(args: Namespace):
 
     global db
     gi = set_up_galaxy_instance(args.galaxy_url, args.galaxy_key)
-
-    client = MongoClient(
-
-    # connect to MongoDB database
-    "mongodb://{}:{}@{}:{}/{}?authSource={}".format(
-    conf.args.database_usr,
-    conf.args.database_password,
-    conf.args.database_host,
-    conf.args.database_port,
-    conf.args.database_name,
-    conf.args.database_auth_source,
-        )
-    )
-
+    loop.run_until_complete(db.connection())
 
     # delete all data before the new import
     loop.run_until_complete(db.clear_database())
@@ -621,9 +650,7 @@ def command_rebuild(args: Namespace):
     # load data from beacon histories
     for history_id in get_beacon_histories(gi):
         for dataset in get_datasets(gi, history_id):
-            # dataset import happens here
             logging.info(f"next file is {dataset.name}")
-
             # destination paths for downloaded dataset and metadata
             analyses_file = f"/tmp/analyses-{dataset.uuid}"
             biosamples_file = f"/tmp/biosamples-{dataset.uuid}"
@@ -632,21 +659,21 @@ def command_rebuild(args: Namespace):
             genomicVariations_file = f"/tmp/genomicVariations-{dataset.uuid}"
             individuals_file = f"/tmp/individuals-{dataset.uuid}"
             runs_file = f"/tmp/runs-{dataset.uuid}"
-            
 
             BFF_files = [analyses_file, biosamples_file, cohorts_file, datasets_file, genomicVariations_file, individuals_file, runs_file]
             for dataset_file in BFF_files:
-              download_dataset(gi, dataset, dataset_file)
-            
+                download_dataset(gi, dataset, dataset_file)
+
             # download dataset from galaxy
-            #download_dataset(gi, dataset, dataset_file)
-            #prepare_metadata_file(dataset, metadata_file)
-            #BFF_files = [analyses_file, biosamples_file, cohorts_file, datasets_file, genomicVariations_file, individuals_file, runs_file]
+            # download_dataset(gi, dataset, dataset_file)
+            # prepare_metadata_file(dataset, metadata_file)
+            # BFF_files = [analyses_file, biosamples_file, cohorts_file, datasets_file, genomicVariations_file, individuals_file, runs_file]
             import_to_mongodb(BFF_files)
 
             # save the origin of the variants in beacon database
             if args.store_origins:
-                loop.run_until_complete(persist_variant_origins(dataset.id, VCF(dataset_file), variant_origins_file))
+                loop.run_until_complete(persist_variant_origins(dataset.id, genomicVariations_file, variant_origins_file))
+                # loop.run_until_complete(persist_variant_origins(dataset.id, VCF(dataset_file), variant_origins_file))
 
     # calculate variant counts
     logging.info("Setting variant counts")
@@ -667,19 +694,35 @@ def command_search(args: Namespace):
     for history_id in get_beacon_histories(gi):
         for dataset in get_datasets(gi, history_id):
 
-            dataset_file = f"/tmp/searching-{dataset.uuid}"
-            download_dataset(gi, dataset, dataset_file)
+            analyses_file = f"/tmp/analyses-{dataset.uuid}"
+            biosamples_file = f"/tmp/biosamples-{dataset.uuid}"
+            cohorts_file = f"/tmp/cohorts-{dataset.uuid}"
+            datasets_file = f"/tmp/ datasets-{dataset.uuid}"
+            genomicVariations_file = f"/tmp/genomicVariations-{dataset.uuid}"
+            individuals_file = f"/tmp/individuals-{dataset.uuid}"
+            runs_file = f"/tmp/runs-{dataset.uuid}"
+            BFF_files = [analyses_file, biosamples_file, cohorts_file, datasets_file, genomicVariations_file, individuals_file, runs_file]
+            for dataset_file in BFF_files:
+                download_dataset(gi, dataset, dataset_file)
 
-            dataset_vcf: VCF
-            dataset_vcf = VCF(dataset_file)
+            # dataset_vcf: VCF
+            # dataset_vcf = VCF(dataset_file)
+            with open(genomicVariations_file) as j_f:
+                data = json.load(j_f)
+                for variant in data:
+                    ALT = variant['alternateBases']
+                    start = variant['position']['start'][0]
+                    REF = variant['referenceBases']
+                    if start == args.start and REF == args.ref and args.alt in ALT:
+                        print(f"found variant in dataset {dataset.id} ({dataset.name})")
 
-            variant: Variant
-            for variant in dataset_vcf:
-
-                if variant.start == args.start and variant.REF == args.ref and args.alt in variant.ALT:
-                    print(f"found variant in dataset {dataset.id} ({dataset.name})")
-
-            os.remove(dataset_file)
+            # variant: Variant
+            # for variant in dataset_vcf:
+            #
+            #     if variant.start == args.start and variant.REF == args.ref and args.alt in variant.ALT:
+            #         print(f"found variant in dataset {dataset.id} ({dataset.name})")
+            for dataset_file in BFF_files:
+                os.remove(dataset_file)
 
 
 def main():
